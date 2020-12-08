00b565c8a8468c09a3a0b77f77461df75e734cf8
Illiachr <74302872+Illiachr@users.noreply.github.com>
2020-12-08 17:27:08 +0200

    DomElement ready stable

---
+++ main.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+'use strict';
+// 1) Сделать класс DomElement, который
+//    содержит свойства
+//   - selector, 
+//   - height,
+//   - width, 
+//   - bg, 
+//   - fontSize
+    const DomElement = function () {
+        this.selector = ''; 
+        this.height = '';
+        this.width = ''; 
+        this.bg = ''; 
+        this.fontSize = '';    
+    };
+
+    DomElement.prototype.createElem = function () {
+        const selectorNorm = this.selector.trim().toLowerCase();
+        let blockType = '';
+        if (this.selector === '') {return console.log('Error: selector is undefined');}
+        if (selectorNorm.charAt(0) === '.') {
+            blockType = 'div';
+        } 
+        if (selectorNorm.charAt(0) === '#') {
+            blockType = 'p';
+        }        
+        const elem = document.createElement(blockType);
+        elem.classList.add(selectorNorm.slice(1));
+        document.body.append(elem);
+        elem.style.height = this.height;
+        elem.style.width = this.width; 
+        elem.style.background = this.bg; 
+        elem.style.fontSize = this.fontSize; 
+        return elem;
+    };
+
+    const block = new DomElement();
+    block.selector = '.app-div';
+    block.width = '100px';
+    block.height = '100px';
+    block.bg = 'orange';
+    block.fontSize = '24px';
+    const appDiv = block.createElem();
+    console.log(block);
+
+    appDiv.textContent = `Hello I am New Division`;
+
+
+});
